60eb8221bb311a694c90d58c0d8d36eec155df04
Brian <blsvsanteliz@gmail.com>
2021-02-14 01:13:48 -0400

    modify resolver to upload image to cloudinary and array of images

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const { ApolloServer, gql } = require("apollo-server");
 const cloudinary = require("cloudinary").v2;
+require("dotenv").config();
 const typeDefs = gql`
   type File {
     filename: String!
-    mimetype: String!
-    encoding: String!
+    path: String!
   }
 
   type Query {
@@ -14,16 +14,23 @@ const typeDefs = gql`
 
   type Mutation {
     singleUpload(file: Upload!): File!
-    arrayUpload(files: [Upload]!): [File]
+    arrayUpload(files: [Upload]!): [File]!
   }
 `;
 /* 
   TODO:
-  * Subir imagenes a clodionary (al menos una)
-  * Subir ina imagen a la catpeta del server (al menos una)
-  * Subit un arreglo de imagenes en el server
+  * Subir imagenes a cloudinary (al menos una) [x]
+  * Subir ina imagen a la carpeta del server (al menos una) 
+  * Subir un arreglo de imagenes en el server
 
-*/
+  */
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+const arrayFiles = [];
+
 const resolvers = {
   Query: {
     uploads: (parent, args) => {},
@@ -31,10 +38,55 @@ const resolvers = {
   },
   Mutation: {
     singleUpload: async (parent, { file }) => {
-      const { filename, mimetype, createReadStream } = await file;
+      const { filename, createReadStream } = await file;
+      try {
+        const result = await new Promise((resolve, reject) => {
+          createReadStream().pipe(
+            cloudinary.uploader.upload_stream((error, imageUpload) => {
+              if (error) return reject(error);
+
+              resolve(imageUpload);
+            })
+          );
+        });
+
+        return {
+          filename,
+          path: result.secure_url,
+        };
+      } catch (err) {
+        console.log(err);
+      }
     },
     arrayUpload: async (parent, { files }) => {
-      console.log(files);
+      await files.map(async (file) => {
+        const { filename, createReadStream } = await file;
+        try {
+          const result = await new Promise((resolve, reject) => {
+            createReadStream().pipe(
+              cloudinary.uploader.upload_stream((error, imageUpload) => {
+                if (error) return reject(error);
+
+                resolve(imageUpload);
+              })
+            );
+          });
+          /*  
+          Momentananeamente se guarda en un array de files ya que es lo que retorna, 
+          estos datos deben guardarse en un arreglo en la base de datos, usando una consulta Cypher 
+          para el file type y asi se vaya creando el arraglo de file
+          */
+          console.log(result);
+          const resultFile = {
+            filename,
+            path: result.secure_url,
+          };
+          arrayFiles.push(resultFile);
+          return arrayFiles;
+        } catch (err) {
+          console.log(err);
+        }
+      });
     },
   },
 };
